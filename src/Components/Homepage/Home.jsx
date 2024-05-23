85f7a7ff33417e4e29b88aaa376f5dcc6e8f790e
Muhammad Asharib <132045877+Asharib15@users.noreply.github.com>
2024-05-24 00:29:43 +0500

    Initial commit

---
+++ src/Components/Homepage/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+
+const Home = () => {
+  return (
+    <div className="home">
+        <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+  
+</nav>
+
+<nav class="navbar bg-primary" data-bs-theme="dark">
+  
+</nav>
+
+<nav class="navbar" style="background-color: #e3f2fd;">
+  
+</nav>
+        
+      
+    </div>
+  )
+}
+
+export default Home
